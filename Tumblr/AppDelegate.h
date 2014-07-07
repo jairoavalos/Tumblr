ddb8b763630dfc68b26b39c86e95b336bb57fcf3
jairoavalos <jairoavalos@Jairos-Gametime-MacBook-Air.local>
2014-07-06 18:52:26 -0700

    Initial Commit

---
+++ Tumblr/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Tumblr
+//
+//  Created by Jairo Avalos on 7/6/14.
+//  Copyright (c) 2014 Jairo Avalos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
